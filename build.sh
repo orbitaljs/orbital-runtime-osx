9b8369c6c70d0e59cbcdff2a5f97088d292b6d63
Matt Mastracci <matthew@mastracci.com>
2015-06-07 17:01:11 -0600

    Download electron shell, fix travis again

--- build.sh
+++ build.sh
@@ -8,14 +8,26 @@ mkdir _tmp
 
 if [ -f _dl/jdk-8u45-macosx-x64.dmg ];
 then
-	echo "Already downloaded, skipping"
+	echo "OSX JDK already downloaded, skipping"
 else
 	wget -P _dl --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie;" \
 		http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-macosx-x64.dmg
 fi
 
+if [ -f _dl/electron-v0.27.2-darwin-x64.zip ];
+then
+	echo "Electron Shell already downloaded, skipping"
+else
+	wget -P _dl https://github.com/atom/electron/releases/download/v0.27.2/electron-v0.27.2-darwin-x64.zip
+fi
+
 cd _tmp
 rm log || true
+
+echo ==================
+echo Extracting OSX JDK
+echo ==================
+echo
 
 echo Extracting outer DMG
 7z -y x ../_dl/jdk*.dmg >> log
@@ -37,3 +49,10 @@ echo Extracting JDK sub-pkg 2/2
 7z -y -ojdk x Payload~ >> log
 
 rm Payload*
+
+echo
+echo ===================
+echo Extracting Electron
+echo ===================
+echo
+
