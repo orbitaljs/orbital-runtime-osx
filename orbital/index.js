8688c1b139f546b5649c87beee940fa5353771db
Matt Mastracci <matthew@mastracci.com>
2015-06-08 21:23:46 -0600

    Add protocol handler

--- orbital/index.js
+++ orbital/index.js
@@ -1,6 +1,8 @@
 var jvm = require('bindings')('jvm');
 var rpc = require('./rpc');
 var path = require('path');
+var process = require('process');
+var app = require('app');
 
 var initialized = false;
 
@@ -16,13 +18,48 @@ function initialize(options) {
 
 	initialized = true;
 
-	var jvmPath = path.join(module.filename, "../../../../Java/lib/server/libjvm.dylib");
-	console.log(jvmPath);
-	jvm.load(jvmPath);
-	jvm.init("-verbose:class", "-Djava.class.path=/tmp/runtime-osx/orbital-java/target/orbital-app-0.1.jar");
-	jvm.run(options.main);
+	// If we've been passed the PIPE environment variable, that means we're in inverted mode
+	if (!process.env.PIPE) {
+		console.log("Initializing JVM");
+		try {
+			var jvmPath = path.join(module.filename, "../../../../Java/lib/server/libjvm.dylib");
+			console.log(jvmPath);
+			jvm.load(jvmPath);
+			jvm.init("-verbose:class", "-Djava.class.path=/tmp/runtime-osx/orbital-java/target/orbital-app-0.1.jar");
+			jvm.run(options.main);
+		} catch (e) {
+			console.log("Failed to initialize the JVM", e);
+			throw e;
+		}
+	}
 
-	rpc.start();
+	// Start RPC
+	try {
+		rpc.start();
+	} catch (e) {
+		console.log("Failed to initialize RPC", e);
+		throw e;
+	}
+
+	// Set up protocol handler
+	app.on('ready', function() {
+		var rootWebPath = process.env.WEB_PATH 
+			? process.env.WEB_PATH 
+			: path.join(module.filename, "../../../../Resources/web");
+		console.log("Web path root: ", rootWebPath);
+
+		var protocol = require('protocol');
+		protocol.registerProtocol('app', function(request) {
+			var url = request.url.slice(4);
+			if (url.slice(-1) == '/')
+				url += "index.html";
+			if (url.indexOf('#') != -1)
+				url = url.slice(0, url.indexOf('#'));
+			var file = path.normalize(rootWebPath + '/' + url);
+			console.log(file);
+			return new protocol.RequestFileJob(file);
+		});
+	}); 
 }
 
 module.exports = {
