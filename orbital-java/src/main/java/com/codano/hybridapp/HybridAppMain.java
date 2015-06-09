68bb4990f33e9d50e4ac176f90ca342f528c9edf
Matt Mastracci <matthew@mastracci.com>
2015-06-08 21:23:57 -0600

    Java code was extracted

--- orbital-java/src/main/java/com/codano/hybridapp/HybridAppMain.java
+++
@@ -1,7 +0,0 @@
-package com.codano.hybridapp;
-
-public class HybridAppMain {
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-	}
-}
--- orbital-java/src/main/java/com/codano/hybridapp/PacketCoderException.java
+++
@@ -1,8 +0,0 @@
-package com.codano.hybridapp;
-
-@SuppressWarnings("serial")
-public class PacketCoderException extends RuntimeException {
-	public PacketCoderException(Throwable src) {
-		super(src);
-	}
-}
